a9c28d1e73fa48871fa325fb50010466d9531ded
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2024-05-07 20:07:13 +0300

    Add Project

---
+++ ProductMicroservicesProject/ProductMicroservicesProject/Models/ProductDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductMicroservicesProject.Models
+{
+    public class ProductDBContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options) { }
+    }
+}
---
+++ ProductMicroservicesProject/ProductMicroservicesProject/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductMicroservicesProject.Models;
+
+namespace ProductMicroservicesProject.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int productid);
+        
+        void InsertProduct(Product product);
+        void DeleteProduct(int productid);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
